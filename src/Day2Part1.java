b3740b0f718546e5277e1ac20a012725f096195f
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-10 16:48:08 +0200

    Refactor Day2Part1 en cours

--- src/Day2Part1.java
+++ src/Day2Part1.java
@@ -6,16 +6,19 @@ import java.util.List;
 
 public class Day2Part1 {
     public static void main(String[] args) {
-        List<Game> games = readFile("ressources/gameCube.txt");
+        List<String> lines = Utils.readFile("ressources/gameCube.txt");
+        List<Game> games = new ArrayList<>();
+
+        for (String line : lines) {
+            games.add(getGameFromString(line));
+        }
 
         int sumId = 0;
         for (Game game : games) {
             if (game.red <= 12 && game.green <= 13 && game.blue <= 14)
-               sumId += game.id ;
+                sumId += game.id;
         }
-
         System.out.println(sumId);
-
     }
 
     public static class Game {
@@ -28,44 +31,67 @@ public class Day2Part1 {
         }
     }
 
-    public static List<Game> readFile(String path)  {
-        List<Game> games = new ArrayList<>();
+    public static class Set {
+        int red;
+        int green;
+        int blue;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line;
-            while ((line = br.readLine()) != null) {
+        public Set() {
+        }
+    }
 
-                String[] splitId = line.split(":");
-                Game game = new Game();
-                game.id = Integer.parseInt(splitId[0].substring(5));
+    public static Game getGameFromString(String line) {
+        Game game = new Game();
+        char[] input = line.toCharArray();
+        
+        String gameId = "";
+        List<Set> gameSets = new ArrayList<>();
 
-                String[] splitSets = splitId[1].split(";");
 
-                for (String set : splitSets) {
-                    String[] splitColors = set.split(",");
-                    for (String color : splitColors) {
-                        color = color.trim();
+        for (int inputIndex = 0; inputIndex < input.length; inputIndex++) {
+            if
 
-                        String[] parts = color.split(" ");
-                        int count = Integer.parseInt(parts[0]);
 
-                        if ("red".equals(parts[1])) {
-                            if (game.red < count) game.red = count;
-                        }
-                        else if ("green".equals(parts[1])) {
-                            if (game.green < count) game.green = count;
-                        }
-                        else if ("blue".equals(parts[1])) {
-                            if (game.blue < count) game.blue = count;
-                        }
-                        }
-                }
-                games.add(game);
+
+
+
+            if (input[inputIndex] == ':') {
+                inputIndex++;
+                break;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        
+
+
         }
 
-        return games;
+
+        String[] splitId = line.split(":");
+
+        game.id = Integer.parseInt(splitId[0].substring(5));
+
+        String[] splitSets = splitId[1].split(";");
+
+        for (String set : splitSets) {
+            String[] splitColors = set.split(",");
+            for (String color : splitColors) {
+                color = color.trim();
+
+                String[] parts = color.split(" ");
+                int count = Integer.parseInt(parts[0]);
+
+                if ("red".equals(parts[1])) {
+                    if (game.red < count) game.red = count;
+                } else if ("green".equals(parts[1])) {
+                    if (game.green < count) game.green = count;
+                } else if ("blue".equals(parts[1])) {
+                    if (game.blue < count) game.blue = count;
+                }
+            }
+        }
+
+
+        return game;
     }
 }
+

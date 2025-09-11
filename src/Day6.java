b66ff5c221134c364db8144dd07a988718a088af
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-11 16:50:39 +0200

    Day6 part 2

--- src/Day6.java
+++ src/Day6.java
@@ -5,42 +5,32 @@ public class Day6 {
         Race race2 = new Race(92, 1064);
         Race race3 = new Race(97, 1505);
         Race race4 = new Race(90, 1100);
-        int margin = race1.chancesToWin * race2.chancesToWin * race3.chancesToWin * race4.chancesToWin;
+        long margin = race1.chancesToWin * race2.chancesToWin * race3.chancesToWin * race4.chancesToWin;
         System.out.println(margin);
 
-        new Race(7,9).getSolution();
-
-        Race race5 = new Race(40929790, 215106415051100);
+        Race race5 = new Race(40929790, 215106415051100L);
+        System.out.println(race5.chancesToWin);
 
     }
 
     public static class Race {
-        int time;
+        long time;
         long record;
-        int chancesToWin;
+        long chancesToWin;
 
-        public Race(int time, long record) {
+        public Race(long time, long record) {
             this.time = time;
             this.record = record;
             this.chancesToWin = getChancesToWin();
         }
 
-        public int getChancesToWin() {
-            int counter = 0;
+        public long getChancesToWin() {
+            long counter = 0;
             for (int t = 1; t < time; t++) {
                 if (record < t * (time - t))
                    counter++;
             }
             return counter;
-        }
-
-        public void getSolution() {
-            long delta = (long) time * time + 4*record;
-            double sqrt = Math.sqrt(delta);
-            double x1 = (time - sqrt) / 2;
-            double x2 = (time + sqrt) / 2;
-
-            System.out.println(x1 + " " + x2);
         }
     }
 }

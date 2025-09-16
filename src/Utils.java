1ca748b3488eb36c3622fcb3afc0aa8facab9ff8
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-16 13:49:22 +0200

    Day 8 part 2: add utility method to compare character arrays

--- src/Utils.java
+++ src/Utils.java
@@ -186,4 +186,12 @@ public class Utils {
         }
         return sum;
     }
+
+    public static boolean isSame(char[] arr1, char[] arr2) {
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0 ; i < arr1.length ; i++){
+            if (arr1[i]!= arr2[i]) return false;
+        }
+        return true;
+    }
 }
